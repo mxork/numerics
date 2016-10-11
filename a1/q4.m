7d014b1ee5de159fa1a8c4ef5e9aedf00871caac
Dan Anderson <dan@packer-debian-8-amd64.digitalocean.com>
2016-10-11 19:44:22 +0000

    fuck q4, again

--- a1/q4.m
+++ a1/q4.m
@@ -7,6 +7,14 @@ function unused = q4()
 		@(x) (1+25.*x.^2).^(-1)
 	};
 
+	funcnames = {
+		"sign",
+		"sin",
+		"abs",
+		"quintic",
+		"cauchy"
+	}
+
 	intmethods = {
 		'nearest',
 		'linear',
@@ -15,12 +23,11 @@ function unused = q4()
 		'cubic'
 	};
 
-	N = arrayfun(@(x) 2.^x, 1:6);
-	H = arrayfun(@(x) 2./x, N);
+	N = arrayfun(@(x) 2.^x, 2:8)
+	H = arrayfun(@(x) 2./x, N)
 
 	for fi = 1:length(funcs)
-
-		G = linspace(-1, 1, 1000);
+		G = linspace(-1, 1, 10000);
 		Exact = funcs{fi}(G);
 		Errors = zeros(length(intmethods), length(H));
 
@@ -30,16 +37,15 @@ function unused = q4()
 				Sampled = funcs{fi}(X);
 				Apprx = interp1(X, Sampled, G, intmethods{mi});
 
-				errs = abs(Exact .- Apprx);
+				errs = abs(Exact - Apprx);
 				Errors(mi, ni) = max(errs);
-
-				plot(G, Apprx, G, Exact);
-				print();
 			end
 		end
 
-%		loglog(H, Errors);
-%		print(["q4plots/", num2str(fi), ".png"] , "-dpng");
+		funcnames{fi}
+		Errors
+		loglog(H, Errors);
+		print(["q4plots/", funcnames{fi}, ".png"] , "-dpng");
 	end
 end
 
