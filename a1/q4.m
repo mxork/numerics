b2d9a57a9da87b32acc0abe01ac750cb75290c51
Dan Anderson <dan@packer-debian-8-amd64.digitalocean.com>
2016-10-11 19:22:27 +0000

    fuck q4

--- a1/q4.m
+++ a1/q4.m
@@ -5,31 +5,42 @@ function unused = q4()
 		@(x) abs(x),
 		@(x) x.^5,
 		@(x) (1+25.*x.^2).^(-1)
-	}
+	};
 
 	intmethods = {
-		"nearest",
-		"linear",
-		"parabolic",
-		"spline",
-		"pchip",
-		"cubic"
-	}
+		'nearest',
+		'linear',
+		'spline',
+		'pchip',
+		'cubic'
+	};
 
-	G = linspace(-1,1, 1000);
-	N = arrayfun(@(x) 2.^x, 1:6)
-	H = arrayfun(@(x) 2./x, N)
+	N = arrayfun(@(x) 2.^x, 1:6);
+	H = arrayfun(@(x) 2./x, N);
 
 	for fi = 1:length(funcs)
+
+		G = linspace(-1, 1, 1000);
+		Exact = funcs{fi}(G);
+		Errors = zeros(length(intmethods), length(H));
+
 		for mi = 1:length(intmethods)
 			for ni = 1:length(N)
-				Exact = funcs{fi}(G)
-				X = linspace(-1, 1, N)
-				Sampled = funcs{fi}(X
-				Apprx = interp1(X, Sampled, G, intmethods(mi))
+				X = linspace(-1, 1, N);
+				Sampled = funcs{fi}(X);
+				Apprx = interp1(X, Sampled, G, intmethods{mi});
 
-				Errors = abs(Exact .- Apprx)
+				errs = abs(Exact .- Apprx);
+				Errors(mi, ni) = max(errs);
+
+				plot(G, Apprx, G, Exact);
+				print();
 			end
 		end
+
+%		loglog(H, Errors);
+%		print(["q4plots/", num2str(fi), ".png"] , "-dpng");
 	end
 end
+
+q4();
