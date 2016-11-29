b7e49be2b3696e4faad23deb0608f3517ddf1bd8
Dan Anderson <dan.hillier.anderson@gmail.com>
2016-11-29 12:32:17 -0500

    rearrange matrix solve

--- a2/question1.jl
+++ a2/question1.jl
@@ -2,11 +2,11 @@ using PyPlot
 
 # question 1.1.4; little miffed.
 function question11()
-    Xs = [2.0^p for p in -7:-2]
-    Ts = [2.0^p for p in -7:-2]
+    Xs = [2.0^p for p in -10:-2]
+    Ts = [2.0^p for p in -10:-2]
     Ths = [x for x in (0:4)/4.0]
 
-    timer = function (dx, dt)
+    timer = function (dx::Float64, dt::Float64)
         tic();
         err = q11error(dx,dt,1/2)
         t = toc()
@@ -16,20 +16,20 @@ function question11()
     EffErr = reshape( pmap( tuple -> timer(tuple...), [ (dx, dt) for dx in Xs, dt in Ts]), size(Xs * Ts') )
 
     clf()
-    contourf( log.([dx for dx in Xs, dt in Ts]), log.([dt for dx in Xs, dt in Ts]), log.(EffErr))
+    contourf( log.([dx for dx in Xs, dt in Ts]), log.([dt for dx in Xs, dt in Ts]), log.(EffErr), cmap=ColorMap("inferno") )      
     xlabel("log delta-x")
     ylabel("log delta-t")
 end
 
-
-@everywhere function gcn(u, dx, dt, theta)
+# obsoleted
+@everywhere function gcn(u::Array{Float64,1}, dx::Float64, dt::Float64, theta::Float64)
     # V = u^n+1
     # Av = Bu
     omega = dt/(dx^2)
     N = length(u)
 
     # return argument mod N, guaranteed to be in [0,N-1]
-    function wrapmod(i)
+    function wrapmod(i::Int64)
         x = i%N
         if x >= 0
             return x
@@ -60,10 +60,11 @@ end
     v = A\(B*u)
 end
 
-@everywhere function q11error(Δx,Δt,θ)
+@everywhere function q11error(Δx::Float64, Δt::Float64, θ::Float64)
     N = round(Int, 1/Δt)
     M = round(Int, 2*pi/Δx)
-    Ω = Δt / (Δx)^2
+    omega = Ω = Δt / (Δx)^2
+    theta = θ
 
     X = (1:M)*Δx
 
@@ -73,8 +74,38 @@ end
 
     u = u0
 
+    function wrapmod(i::Int64)
+        x = i%M
+        if x >= 0
+            return x
+        else
+            return x+( (-div(x, M)+1)*M)
+        end
+    end
+
+    # the spatial stencil
+    stencil = function(i,j)
+        k = wrapmod(i-j)
+        if k==0
+            return -2
+        elseif k==1 || k==M-1
+            return 1
+        else
+            return 0
+        end
+    end
+
+
+    # d2 matrix
+    Dxx = [stencil(i,j) for i in 0:(M-1), j in 0:(M-1)]
+
+    A = eye(M) - theta*omega*Dxx
+    B = eye(M) + (1-theta)*omega*Dxx
+
+    C = A\B
+
     for i = 1:N
-        u = gcn(u,Δx,Δt,θ)
+        u = C*u
     end
 
     Ans = u
@@ -90,3 +121,5 @@ end
 function q12error()
 
 end
+
+question11()
